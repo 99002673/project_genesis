70f606bca6541781b81a8e83e6b3ec5480ec770b
99002673 <sachinc.mirji@ltts.com>
2020-09-19 10:26:34 +0530

    include

---
+++ inc/phy_equation.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include "phy_equation.h"
+int ohms_law();
+int velocity_dis_time();
+int kinetic_energy();
+int force();
+int potential_energy();
+int power();
+int density();		
+int pressure();
